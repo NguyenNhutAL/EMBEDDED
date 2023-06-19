c7e0eaf33d3f617a6f35a90352d73ae557cdc6a3
NguyenNhutAL <131443345+NguyenNhutAL@users.noreply.github.com>
2023-06-19 15:02:02 +0700

    Update master_spi.ino

--- master_spi.ino
+++ master_spi.ino
@@ -1,58 +1,82 @@
-/*
- *Author: Nguyen Minh Nhut
- *Date: 18/06/2023
- *Description: this program sent to slave with spi standard
-*/
+#define SCK  4 
+#define MOSI 5  
+#define MISO 6  
+#define SS   7  
 
-#define SCK_PIN    4
-#define MOSI_PIN   5
-#define MISO_PIN   6
-#define SS_PIN     7
+bool stateSlave = 1;
+uint8_t dataReceived = 0x00;
+uint8_t sizeDataReceived = 0;
+uint8_t dataTransmited = 0x00;
+uint8_t sizeDataTransmited = 0;
 
-void SPI_Soft_Init(void);
-void SPI_Soft_Transmit(uint8_t mData);
+void spiSoftTransmit();
+void spiSoftReceived();
+void spiSoftSlave();
 
-
-void setup() {
-  SPI_Soft_Init();
-}
-
-void loop() {
-  digitalWrite(SS_PIN,LOW);
-  SPI_Soft_Transmit('A');
-  digitalWrite(SS_PIN,HIGH);
-  delay(2000);
-}
-
-
-void SPI_Soft_Init()
+void setup()
 {
-  pinMode(SCK_PIN,OUTPUT);
-  pinMode(MOSI_PIN,OUTPUT);
-  pinMode(MISO_PIN,INPUT);
-  pinMode(SS_PIN , OUTPUT);
-  digitalWrite(SCK_PIN,LOW);
-  digitalWrite(SS_PIN,HIGH);
+  Serial.begin(115200);
+  pinMode(SCK, INPUT);
+  pinMode(MOSI, INPUT);
+  pinMode(SS, INPUT);
+  pinMode(MISO, OUTPUT);
 }
 
-
-void SPI_Soft_Transmit(uint8_t mData)
+void loop()
 {
-  uint8_t temp=0;
-  for(int i=0;i<8;i++){
-     temp= mData & 0x80 ;
-     if(temp)
-     {
-        digitalWrite(MOSI_PIN,HIGH);
-     }
-     else
-     {
-        digitalWrite(MOSI_PIN,LOW);
-     }
-     delayMicroseconds(25);
-     digitalWrite(SCK_PIN,HIGH);
-     delayMicroseconds(25);
-     digitalWrite(SCK_PIN,LOW);
-     mData=mData<<1;
+  if (digitalRead(SS) == HIGH) {
+    if (sizeDataReceived == 8) {
+      Serial.print("Data Received from Master: ");
+      Serial.println((char)dataReceived);
+      dataReceived = 0x00;
+      sizeDataReceived = 0;
+      stateSlave = 0;
+      dataTransmited = 88;
+    }
+    else
+    {
+      dataReceived = 0x00;
+      sizeDataReceived = 0;
+    }
+    if (sizeDataTransmited == 8) {
+      dataTransmited = 88;
+      sizeDataTransmited = 0;
+      stateSlave = 1;
+      Serial.println("Transmited Data");
+    }
+    else
+    {
+      sizeDataTransmited = 0;
+    }
   }
+}
+
+
+void spiSoftSlave()
+{
+  if (digitalRead(SS) == HIGH) return;
+  if (stateSlave)
+    spiSoftReceived();
+  else
+    spiSoftTransmit();
+}
+
+void spiSoftReceived()
+{
+  dataReceived = dataReceived << 1;
+  dataReceived = dataReceived | digitalRead(MOSI);   
+  delay(2);
+  sizeDataReceived++;
+}
+
+void spiSoftTransmit()
+{
+  uint8_t x = 0;
+  x = dataTransmited & 0x80; 
+  if (x > 0)
+    digitalWrite(MISO, HIGH);
+  else
+    digitalWrite(MISO, LOW);
+  dataTransmited = dataTransmited << 1; 
+  sizeDataTransmited++;
 }
